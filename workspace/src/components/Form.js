03f25c6866843492af7ccefc5fd8867ab36213bc
rene <renegmed@yahoo.com>
2018-12-05 19:56:19 -0500

    updated form view - incorporate action properties

--- workspace/src/components/Form.js
+++ workspace/src/components/Form.js
@@ -2,36 +2,39 @@ import React, { Component } from 'react';
 
 
 export default class Form extends Component {
-  handleChange(event) {
-    this.props.handleChange(event);
-  }
-  handleSubmit(event) {
-    this.props.handleSubmit(event);
-  }
+  
   render() {
-    return (
+    return (      
       <div>
-        <form onSubmit={this.props.handleSubmit} className="col s12">
+        <form 
+          onSubmit={event => {
+            event.preventDefault();
+            const note = {
+              id: require('crypto').randomBytes(5).toString('hex'),
+              title: this.refs.title.value,
+              details: this.refs.details.value
+            }
+            this.refs.title.value = '';
+            this.refs.details.value = '';
+            this.props.addNewNote(note);
+        }} className="col s12">
           <div className="row">
             <div className="input-field col s3">
-              <input 
-                id="title"
+              <input
+                id="title" 
                 name="currentTitle"
                 type="text" 
-                value={this.props.currentTitle} 
-                onChange={this.props.handleChange} 
-                className="validate"/>
+                ref="title"
+                className="validate"/> 
                 <label htmlFor="title">Title</label>
             </div>
             <div className="input-field col s7">
               <input
                 id="details"
                 name="currentDetails"
-                type="text" 
-                value={this.props.currentDetails} 
-                onChange={this.props.handleChange}
-                className="validate" />
-              <label htmlFor="details">Details</label>
+                type="text"                 
+                ref="details" />
+                <label htmlFor="details">Details</label>
             </div>
             <div className="input-field col s2">
                 <button className="btn-large waves-effect waves-light" 
